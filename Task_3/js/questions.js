c1fbccb574614225bc902e0ddb67ceba02ca1500
akanksha8149 <119891477+akanksha8149@users.noreply.github.com>
2023-02-21 00:06:21 +0530

    Add files via upload

---
+++ Task_3/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The correct sequence of HTML tags for starting a webpage is -",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+     "HTML, Head, Title, Body",
+      "HTML, Head, Title, Body"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is the correct syntax to make the background-color of all paragraph elements to yellow?",
+    answer: "p {background-color : yellow;}",
+    options: [
+      "p {background-color : yellow;}",
+"p {background-color : #yellow;}",
+"all {background-color : yellow;}",
+"all p {background-color : #yellow;}"
+]
+  },
+    {
+    numb: 5,
+    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answer: "ignores the statements",
+    options: [
+      "throws an error",
+      "ignores the statements",
+      "gives a warning",
+      "none of the above"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
